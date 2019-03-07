9bf45a43f275a8ded17386ad21692c9159a9c79e
bchen111 <kaleidliner0109@yahoo.co.jp>
2019-03-07 18:19:32 -0500

    Reformat code

--- MazeMain.py
+++ MazeMain.py
@@ -5,6 +5,7 @@ from MazeMainWindowUI import Ui_MainWindow
 
 from PyQt5.QtWidgets import QMainWindow, QApplication
 
+
 class MazeMainWindow(QMainWindow, Ui_MainWindow):
 	def __init__(self):
 		super(MazeMainWindow, self).__init__()
@@ -12,7 +13,8 @@ class MazeMainWindow(QMainWindow, Ui_MainWindow):
 		self.centralwidget.parent().resize(1280, 800)
 
 	def mainFunc_generatorRun(self):
-		self.widget_mazeDisplayMain.generator_run(self.comboBox_selectGenerateMethod.currentIndex(), self.spinBox_mazeSize.value())
+		self.widget_mazeDisplayMain.runNewGenerator(self.comboBox_selectGenerateMethod.currentIndex(),
+													self.spinBox_mazeSize.value())
 
 if __name__ == "__main__":
 	sys.setrecursionlimit(10000)
